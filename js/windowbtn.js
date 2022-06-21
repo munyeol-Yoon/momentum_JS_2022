538736cf1c6571d4439a685a40e2ecefcc60e803
munyeol-Yoon <gmakin36@gmail.com>
2022-06-21 18:36:09 +0900

    2022-06-21-1835
    update

--- js/windowbtn.js
+++ js/windowbtn.js
@@ -1,4 +1,4 @@
-const startbtn = document.querySelector(".footer__btntodo:first-child");
+const startbtn = document.querySelector(".footer__btntodo:first-child i");
 const startmenu = document.querySelector(".hiddenstartmenu");
 
 function buttonCliked(){
