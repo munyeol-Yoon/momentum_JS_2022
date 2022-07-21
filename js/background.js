3bf7ea6e73f46b32aed1864df580ff33d260711e
munyeol-Yoon <gmakin36@gmail.com>
2022-07-21 16:16:27 +0900

    taskbar-update
    incomplete

--- js/background.js
+++ js/background.js
@@ -1,6 +1,8 @@
 const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
 
-const choseImage = images[Math.floor(Math.random() * images.length)]
+const random = Math.floor(Math.random() * images.length);
+
+const choseImage = images[random];
 
 /*
 ==nomad code==
@@ -13,18 +15,26 @@ document.body.appendChild(bgImage);
 
 document.body.style.backgroundImage = "url('./img/"+choseImage+"')";
 
-if(choseImage === 1){
-    document.getElementById('footer').style.backgroundColor = '#d6cfca';
+
+console.log(random);
+
+if(random === 1){
+    document.getElementById('footer').style.backgroundColor = "#d6cfca";
     //#d6cfca
-}else if(choseImage === 2){
-
-}else if(choseImage === 3){
-
-}else if(choseImage === 4){
-
-}else if(choseImage === 5){
-
-}else if(choseImage === 6){
-
+    document.getElementById('footer').style.color = "black";
+}else if(random === 2){
+    document.getElementById('footer').style.backgroundColor = "#3660D5";
+}else if(random === 3){
+    document.getElementById('footer').style.backgroundColor = "#33OCD3";
+}else if(random === 4){
+    document.getElementById('footer').style.backgroundColor = "#5B9CCF";
+    document.getElementById('footer').style.opacity = "0.5";
+}else if(random === 5){
+    document.getElementById('footer').style.backgroundColor = "#100C23";
+}else if(random === 6){
+    document.getElementById('footer').style.backgroundColor = "#100C23";
+}else{
+    document.getElementById('footer').style.backgroundColor = "#d6cfca";
+    document.getElementById('footer').style.color = "black";
 }
 
